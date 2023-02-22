ad1ea6bd465f6ced44cfac86210b07ff4c7776fc
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-02-23 03:36:52 +0530

    Added: Address Book Service Problem

---
+++ src/com/bridgelabz/addressbook/AddressBookService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Service Program");
+    }
+}
